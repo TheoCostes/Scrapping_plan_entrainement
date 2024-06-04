31c32d403a7285ccac0a9a278cd25e211d20ca00
TheoCostes <theo.costes@ynsect.com>
2024-06-04 15:20:49 +0200

    recupération du nom, distance, durée des session de la semaine en cours

--- scrapping.py
+++ scrapping.py
@@ -3,7 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 from bs4 import BeautifulSoup
 import time
 from dotenv import load_dotenv
@@ -31,13 +34,13 @@ password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
 
 # Attendre la redirection et s'assurer que la connexion est établie
-time.sleep(5)  # Ajuster selon la rapidité du site
+time.sleep(20)  # Ajuster selon la rapidité du site
 
 # Naviguer vers la page des séances et récupérer les noms
 driver.get("https://app.campus.coach/")
 
 # Attendre que la page soit complètement chargée
-driver.implicitly_wait(10)  # ou utilisez WebDriverWait pour des attentes explicites
+driver.implicitly_wait(20)  # ou utilisez WebDriverWait pour des attentes explicites
 
 # Extraire le contenu HTML de la page
 page_source = driver.page_source
@@ -45,15 +48,45 @@ page_source = driver.page_source
 # Fermer le navigateur
 driver.quit()
 
+
 # Utiliser BeautifulSoup pour parser le contenu HTML
 soup = BeautifulSoup(page_source, 'html.parser')
 
-# Trouver tous les éléments avec des classes
-elements_with_classes = soup.find_all(class_=True)
+sessions = soup.find_all('div', {'data-testid': 'session-card'})
 
-# Extraire les classes uniques
-classes = set()
-for element in elements_with_classes:
-    class_list = element.get('class')
-    if "campus-ds__Wrapper-sc-niysmc-4" in class_list and "felyqZ" in class_list and len(class_list) <= 2:
-        print(element.h3)
+print(len(sessions))
+
+session_data = []
+
+for session in sessions:
+    title_element = session.find('h3', {'class': 'campus-ds__HeadingItem-sc-niysmc-0'}) 
+    duration_element = (
+        session
+        .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 iyQsnC'})
+        .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 dLqUiH'})
+    )
+    print(duration_element)
+    duration = duration_element.get_text(strip=True) if duration_element else "Durée non trouvée"
+    
+    distance_element = (
+        session
+        .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 jPXRku'})
+        .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 dLqUiH'})
+    )
+    distance = distance_element.get_text(strip=True) if distance_element else "Distance non trouvée"
+
+    title = title_element.get_text(strip=True) if title_element else "Titre non trouvé"
+    # duration = duration_element.get_text(strip=True) if duration_element else "Durée non trouvée"
+    # distance = distance_element.get_text(strip=True) if distance_element else "Distance non trouvée"
+    
+    session_data.append({
+        'title': title,
+        'duration': duration,
+        'distance': distance
+    })
+    print("===")
+
+# Afficher les résultats
+print("Informations des sessions:")
+for session in session_data:
+    print(f"Nom: {session['title']}, Durée: {session['duration']}, Distance: {session['distance']}")
