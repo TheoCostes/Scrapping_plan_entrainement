8150d432d781ca3e24c51df4de4e747cc7bf9bc4
TheoCostes <theo.costes@ynsect.com>
2024-06-11 15:35:43 +0200

    récupération des seance par semaine

--- scrapping.py
+++ scrapping.py
@@ -11,6 +11,7 @@ from bs4 import BeautifulSoup
 import time
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 
@@ -34,59 +35,77 @@ password_field.send_keys(password)
 password_field.send_keys(Keys.RETURN)
 
 # Attendre la redirection et s'assurer que la connexion est établie
-time.sleep(20)  # Ajuster selon la rapidité du site
+time.sleep(30)  # Ajuster selon la rapidité du site
 
 # Naviguer vers la page des séances et récupérer les noms
 driver.get("https://app.campus.coach/")
 
 # Attendre que la page soit complètement chargée
-driver.implicitly_wait(20)  # ou utilisez WebDriverWait pour des attentes explicites
+driver.implicitly_wait(30)  # ou utilisez WebDriverWait pour des attentes explicites
 
-# Extraire le contenu HTML de la page
-page_source = driver.page_source
+séances_par_semaine = {}
+while True:
+    try:
+         
+        # Extraire le contenu HTML de la page
+        page_source = driver.page_source
 
-# Fermer le navigateur
+        # Utiliser BeautifulSoup pour parser le contenu HTML
+        soup = BeautifulSoup(page_source, 'html.parser')
+
+        sessions = soup.find_all('div', {'data-testid': 'session-card'})
+
+        print(len(sessions))
+        date_semaine = (soup
+                        .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 campus-ds__LabelWrapper-sc-niysmc-133 cxwPQm fKUpVE'})
+                        .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 gYRlMy'})).get_text(strip=True).split(' ')[-1]
+        print(date_semaine)
+        session_data = []
+
+        for session in sessions:
+            date_element = session.find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 fTXqgI'})
+            title_element = session.find('h3', {'class': 'campus-ds__HeadingItem-sc-niysmc-0'}) 
+            duration_element = (
+                session
+                .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 iyQsnC'})
+                .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 dLqUiH'})
+            )
+            print(duration_element)
+            duration = duration_element.get_text(strip=True) if duration_element else "Durée non trouvée"
+            
+            distance_element = (
+                session
+                .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 jPXRku'})
+                .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 dLqUiH'})
+            )
+            distance = distance_element.get_text(strip=True) if distance_element else "Distance non trouvée"
+
+            title = title_element.get_text(strip=True) if title_element else "Titre non trouvé"
+            # duration = duration_element.get_text(strip=True) if duration_element else "Durée non trouvée"
+            # distance = distance_element.get_text(strip=True) if distance_element else "Distance non trouvée"
+            print(date_element.get_text(strip=True))
+            session_data.append({
+                'title': title,
+                "date": date_element.get_text(strip=True),
+                'duration': duration,
+                'distance': distance
+            })
+            print("===")
+
+        # Afficher les résultats
+        print("Informations des sessions:")
+        for session in session_data:
+            print(f"Nom: {session['title']}, Durée: {session['duration']}, Distance: {session['distance']}")
+        séances_par_semaine[date_semaine] = session_data
+        button = driver.find_element(By.CLASS_NAME, 'campus-ds__ControlButton-sc-niysmc-130.campus-ds__ControlButtonNext-sc-niysmc-132.eNcEjV.dbvJXk')
+        if 'disabled' not in button.get_attribute('class'):  # Vérifier si le bouton n'a pas la classe 'disabled'
+            button.click()
+        else:
+            break
+    except:
+        break
+
+with open('séances.json', 'w') as f:
+    json.dump(séances_par_semaine, f)
+
 driver.quit()
-
-
-# Utiliser BeautifulSoup pour parser le contenu HTML
-soup = BeautifulSoup(page_source, 'html.parser')
-
-sessions = soup.find_all('div', {'data-testid': 'session-card'})
-
-print(len(sessions))
-
-session_data = []
-
-for session in sessions:
-    title_element = session.find('h3', {'class': 'campus-ds__HeadingItem-sc-niysmc-0'}) 
-    duration_element = (
-        session
-        .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 iyQsnC'})
-        .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 dLqUiH'})
-    )
-    print(duration_element)
-    duration = duration_element.get_text(strip=True) if duration_element else "Durée non trouvée"
-    
-    distance_element = (
-        session
-        .find('div', {'class': 'campus-ds__Wrapper-sc-niysmc-4 jPXRku'})
-        .find('p', {'class': 'campus-ds__TextElement-sc-niysmc-1 dLqUiH'})
-    )
-    distance = distance_element.get_text(strip=True) if distance_element else "Distance non trouvée"
-
-    title = title_element.get_text(strip=True) if title_element else "Titre non trouvé"
-    # duration = duration_element.get_text(strip=True) if duration_element else "Durée non trouvée"
-    # distance = distance_element.get_text(strip=True) if distance_element else "Distance non trouvée"
-    
-    session_data.append({
-        'title': title,
-        'duration': duration,
-        'distance': distance
-    })
-    print("===")
-
-# Afficher les résultats
-print("Informations des sessions:")
-for session in session_data:
-    print(f"Nom: {session['title']}, Durée: {session['duration']}, Distance: {session['distance']}")
